bf0a8a3c27d4d06ee57422ab08e2dd00deb11757
Rob Thuleen <rob.thuleen@gmail.com>
2016-04-12 19:40:00 -0700

    Test camera

--- pi-cam/index.js
+++ pi-cam/index.js
@@ -45,12 +45,7 @@ PiCam.prototype.startCamera = function (timestamp, sendImage) {
 		var camera = new RaspiCam(cameraOptions);
 		camera.on("read", function(err, timestamp, filename){ 
 			if (err) console.log("ERROR-" + err);
-			var newFileName = filename.slice(0, -1);
-		        setTimeout(function () {
-				console.log("original filename = " + filename);
-				console.log("new filename = " + newFileName);
-				sendImage(imageDir + newFileName);
-			}, 3000);
+			else //sendImage(imageDir + newFileName);
 		});
 		camera.on("start", function () {
 			console.log("Camera started.");
